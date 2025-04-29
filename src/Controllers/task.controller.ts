f61b43d3f1f4a991cbfb1ba9c1be5dc22ca1449c
nicolasgghg <nicolasgghg12@gmail.com>
2025-04-29 08:43:22 -0300

    fix: routes task, authentication is now required to access the route

--- src/Controllers/task.controller.ts
+++ src/Controllers/task.controller.ts
@@ -1,74 +1,135 @@
 import { NextFunction, Request, Response } from "express";
 import { TaskService } from "../Services/task.service";
 import { handleSuccess, handleError } from "./handlesControllers";
-import jose from 'jose'
+import jose from "jose";
 
-declare module 'express-serve-static-core' {
-    interface Request {
-        user?: jose.JWTPayload
-    }
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: jose.JWTPayload;
+  }
 }
 
 export class TaskController {
-    private _taskService: TaskService;
+  private _taskService: TaskService;
 
-    constructor() {
-        this._taskService = new TaskService();
+  constructor() {
+    this._taskService = new TaskService();
+  }
+
+  createTask = async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body;
+    const userId = Number(req.user?.id as number);
+
+    if (!userId) {
+      res
+        .status(400)
+        .json({ message: "Invalid token payload: missing user ID" });
+      return;
     }
 
-    createTask = async (req: Request, res: Response, next: NextFunction) => {
-        if (!req.user) {
-            res.status(401).json({ message: 'Unauthorized: Token does not exist' })
-            return
-        }
+    try {
+      const dataTask = await this._taskService.createTask({
+        ...data,
+        userId: userId,
+      });
+      handleSuccess(res, 201, "Created Successfully", {
+        ...dataTask,
+        password: undefined,
+      });
+    } catch (error) {
+      handleError(res, error, next);
+    }
+  };
 
-        const data = req.body;
+  getAllTasks = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = await this._taskService.getAllTasks();
+      handleSuccess(res, 200, "Success", data);
+    } catch (error) {
+      handleError(res, error, next);
+    }
+  };
 
-        try {
-            const dataTask = await this._taskService.createTask(data);
-            handleSuccess(res, 201, 'Created Successfully', { ...dataTask, password: undefined });
-        } catch (error) {
-            handleError(res, error, next);
-        }
-    };
+  getTaskById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = Number(req.user?.id);
+      if (!userId) {
+        res
+          .status(401)
+          .json({ message: "Unauthorized: missing user ID in token" });
+        return;
+      }
 
-    getAllTasks = async (_req: Request, res: Response, next: NextFunction) => {
-        try {
-            const data = await this._taskService.getAllTasks();
-            handleSuccess(res, 200, "Success", data);
-        } catch (error) {
-            handleError(res, error, next);
-        }
-    };
+      const id = Number(req.params.id);
+      const data = await this._taskService.getTaskById(id);
 
-    getTaskById = async (req: Request, res: Response, next: NextFunction) => {
-        const id = Number(req.params.id);
-        try {
-            const data = await this._taskService.getTaskById(id);
-            handleSuccess(res, 200, "Success", data);
-        } catch (error) {
-            handleError(res, error, next);
-        }
-    };
+      if (data?.userId !== userId) {
+        res
+          .status(403)
+          .json({ message: "Forbidden: you cannot see this task" });
+        return;
+      }
 
-    updateTaskById = async (req: Request, res: Response, next: NextFunction) => {
-        const id = Number(req.params.id);
-        const data = req.body;
-        try {
-            const updatedTaskData = await this._taskService.updateTaskById(id, data);
-            handleSuccess(res, 200, "Task Updated Successfully", updatedTaskData);
-        } catch (error) {
-            handleError(res, error, next);
-        }
-    };
+      handleSuccess(res, 200, "Success", data);
+    } catch (error) {
+      handleError(res, error, next);
+    }
+  };
 
-    deleteTaskById = async (req: Request, res: Response, next: NextFunction) => {
-        const id = Number(req.params.id);
-        try {
-            await this._taskService.deleteTaskById(id);
-            handleSuccess(res, 204)
-        } catch (error) {
-            handleError(res, error, next);
-        }
-    };
+  updateTaskById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = Number(req.user?.id);
+      if (!userId) {
+        res
+          .status(401)
+          .json({ message: "Unauthorized: missing user ID in token" });
+        return;
+      }
+
+      const id = Number(req.params.id);
+      const data = req.body;
+
+      const task = await this._taskService.getTaskById(id);
+
+      if (task?.userId !== userId) {
+        res
+          .status(403)
+          .json({ message: "Forbidden: you cannot modify this task" });
+        return;
+      }
+
+      const updatedTask = await this._taskService.updateTaskById(id, data);
+      handleSuccess(res, 200, "Task Updated Successfully", updatedTask);
+    } catch (error) {
+      handleError(res, error, next);
+    }
+  };
+
+  deleteTaskById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = Number(req.user?.id);
+      if (!userId) {
+        res
+          .status(401)
+          .json({ message: "Unauthorized: missing user ID in token" });
+        return;
+      }
+
+      const id = Number(req.params.id);
+
+      const task = await this._taskService.getTaskById(id);
+
+      if (task?.userId !== userId) {
+        res
+          .status(403)
+          .json({ message: "Forbidden: you cannot Delete this task" });
+        return;
+      }
+
+      await this._taskService.deleteTaskById(id);
+      handleSuccess(res, 204);
+    } catch (error) {
+      handleError(res, error, next);
+    }
+  };
 }
